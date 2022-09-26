05eb33c79dee3ccf4d82756a1a7e977102eca22c
ghaidaahaj4 <gaidaahaj@mail.tau/ac/il>
2022-09-26 23:33:52 +0300

    first

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com"> 
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+<link href="https://fonts.googleapis.com/css2?family=Cairo&family=Cairo+Play&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shope Online || اضافة منتجات</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <center>
+        <div class = "main">
+            <form action="insert.php" method = "post" enctype="multipart/form-data">
+                <h2>موقع تسويقي اونلاين </h2>
+                <img src="/assets/logo.png" alt="logo" width="300px">
+                <br>
+                <input type="text" name='name' placeholder='اسم المنتج' >
+                <br>
+                <input type="text" name='price' placeholder='سعر المنتج'>
+                <br>
+                <textarea  placeholder= "وصف المنتج" id="description" name="description" rows="5" cols="45%"></textarea>
+                <br>
+                <label for="file">اختيار صورة للمنتج</label>
+                <br>
+                <input type="file" id="file" name='image' style='display:none;'>
+                <button name='upload'>رفع المنتج</button>
+                <br>
+                <a herf="products.php">عرض جميع المنتجات</a>    
+            </form>
+        </div>
+        <p> Developed BY BYG</p>
+    </center>
+    
+</body>
+</html>
